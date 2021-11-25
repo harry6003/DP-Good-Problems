0bef5c462b9db964bef8c85663149dcb05057f83
Himanshu <agrawalhimanshu814@gmail.com>
2021-11-25 12:35:45 +0530

    DP method added
    Made calculations for all the smaller problems and then for every medium problem which is dependent on the smaller calls calculated using the relation between them so dont need more repeted calls and hence saved much time and reduced the complexity to o(M*N) where m and n are sizes of the strings given

--- Day 3/6.LCS.cpp
+++ Day 3/6.LCS.cpp
@@ -27,10 +27,40 @@ int lcs(string s, string t)
   }
 }
 
-// Memeoization O(M*N)
+// DP O(M*N)
+
+int lcs_memo(string s, string t, int **dp)
+{
+  for (int i = 0; i <= s.size(); i++)
+  {
+    for (int j = 0; j <= t.size(); j++)
+    {
+      if (i == 0 || j == 0)
+        dp[i][j] = 0;
+      else
+      {
+        if (s[i - 1] == t[j - 1])
+        {
+          dp[i][j] = 1 + dp[i - 1][j - 1];
+        }
+        else
+        {
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+  }
+  return dp[s.size()][t.size()];
+}
 
 int lcs_memo(string s, string t)
 {
+  int **dp = new int *[s.size() + 1];
+
+  for (int i = 0; i <= s.size(); i++)
+    dp[i] = new int[t.size() + 1];
+
+  return lcs_memo(s, t, dp);
 }
 
 int main()
